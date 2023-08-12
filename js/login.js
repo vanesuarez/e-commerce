89d7dfc14590f773c60248d758288b4187f2fe2b
Ignacionande98 <128426701+Ignacionande98@users.noreply.github.com>
2023-08-12 14:22:34 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+const username = document.getElementById("username")
+const passwoord = document.getElementById("passwoord")
+const login = document.getElementById("login-form")
+
+
+login.addEventListener("submit", function () {
+
+    if(username.value != "" && passwoord.value != "") {
+        window.location.href = "./index.html"
+    } else {
+        
+    }
+
+
+
+
+})
