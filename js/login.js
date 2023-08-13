a31342c2d28e0b6fb29f72a32ccf4a04e364c7e0
johanamiranda7 <141370390+johanamiranda7@users.noreply.github.com>
2023-08-13 19:06:59 -0300

    desafiate 1

--- js/login.js
+++ js/login.js
@@ -2,14 +2,16 @@
 const username = document.getElementById("username")
 const passwoord = document.getElementById("passwoord")
 const login = document.getElementById("login-form")
+console.log('tamo aca');
 
-
-login.addEventListener("submit", function () {
-
-    if(username.value != "" && passwoord.value != "") {
-        
-    } else {
-        
+login.addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+    //Redireccionamos al usuario a la portada silas credenciales son validas
+    if(username.value !== "" && passwoord.value !== "") {
+            window.location.href = "index.html";
+    }
+    else {
+        window.location.href = "login.html";
     }
 
 
