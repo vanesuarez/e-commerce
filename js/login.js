40db1fbf2c3391dcd5b4bf9305fb0111a9b17941
Vane Suarez <vanesuarez2301@gmail.com>
2023-08-14 19:59:02 -0300

    Se agego trim para espacios

--- js/login.js
+++ js/login.js
@@ -6,15 +6,15 @@ console.log('tamo aca');
 
 login.addEventListener('submit', function (event) {
     event.preventDefault(); // Evita el envío del formulario por defecto
+
     //Redireccionamos al usuario a la portada silas credenciales son validas
-    if(username.value !== "" && passwoord.value !== "") {
+    if(username.value.trim() !== "" && passwoord.value.trim() !== "") {
             window.location.href = "index.html";
     }
     else {
         window.location.href = "login.html";
     }
-
-
-
-
 })
+
+// localStorage.setItem("user", JSON.stringify(user)); 
+// sessionStorage.getItem("user", JSON.stringify(user));
