92d80eeeef4de1f5e121bc277dadb6d76021cede
Mauricio Machado <133698730+machadokroyen@users.noreply.github.com>
2023-08-23 16:30:32 -0300

    entrega2parte2

--- js/products.js
+++ js/products.js
@@ -1,9 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const productList = document.getElementById("product-list");
 
-    // URL del JSON de productos
-    const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+   // PARTE 2
+/*
+    Por lo que entendí del ejercicio, al elegir una categoría dentro de index.html (Autos, Juguetes, Muebles) se carga en localStorage un valor (101 si accedemos a autos, 102 para juguetes y 103 para muebles).
 
+    Modifiqué el código para que en vez de utilizar una URL fija "https://japceibal.github.io/emercado-api/cats_products/101.json", esta sea dinámica y cambie según la categoría que seleccionemos:
+    
+    "https://japceibal.github.io/emercado-api/cats_products/101.json" para categoría Autos.
+    "https://japceibal.github.io/emercado-api/cats_products/102.json" para categoría Juguetes.
+    "https://japceibal.github.io/emercado-api/cats_products/103.json" para categoría Muebles.
+*/
+
+    const catID = localStorage.getItem("catID"); // obtener la clave de localStorage
+
+    console.log(catID); // prueba, para no acceder a applicartion...
+
+    if (catID) { // si catID es distinto del vacio entonces es true y con ese notenido arma la URL
+        const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+
+    // pasa a estar dentro del if
     // Realizar la petición GET usando Fetch API
     fetch(url)
         .then(response => response.json())
@@ -53,4 +69,5 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch(error => {
             console.error("Error al cargar los productos:", error);
         });
+}
 });
