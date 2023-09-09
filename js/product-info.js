64867314750ea4a92119464af44c99d6d250db2f
johanamiranda7 <141370390+johanamiranda7@users.noreply.github.com>
2023-09-08 23:46:22 -0300

    punto 3 estrellas

--- js/product-info.js
+++ js/product-info.js
@@ -5,8 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // Fetch para cada producto
 
     fetch(url)
-        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          return response.json()
+        })
         .then((data) => {
+          console.log(data);
             document.getElementById("productName").innerHTML = data.name;
             document.getElementById("productPrice").innerHTML = `<b>Precio</b> <br>${data.currency} ${data.cost}`;
             document.getElementById("productDescription").innerHTML = `<b>Descripción</b> <br>${data.description}`;
@@ -45,13 +49,18 @@ document.addEventListener("DOMContentLoaded", function () {
               commentUser.textContent = `Usuario: ${comment.user}`;
 
             //   AGREGAR ACA PUNTAJE CON ESTRELLAS
-        
+              //hago una variable que se llame stars, no constante porque va a variar, x eso variable
+              //no utilice for each porque me parece es una  iteracion para los arrays
+              let stars= ""
+              for(let i =1;i<6;i++){
+                if (i<=comment.score){
+                  stars+='<span class="fa fa-star checked"></span>'
+                } else {
+                  stars+='<span class="fa fa-star"></span>'
+                }
+              }
               const commentScore = document.createElement("p");
-              commentScore.innerHTML = `<span class="fa fa-star checked"></span>
-              <span class="fa fa-star checked"></span>
-              <span class="fa fa-star"></span>
-              <span class="fa fa-star"></span> 
-              <span class="fa fa-star"></span> 
+              commentScore.innerHTML = `${stars} 
               Puntuación: ${comment.score}`;
               
             // -- 
