de643aeb99ea9e4e826c381b937e2805ab49a6fe
vanesuarez <vanesuarez2301@gmail.com>
2023-10-12 21:48:16 -0300

    nav bar updates

--- js/categories.js
+++ js/categories.js
@@ -140,30 +140,5 @@ document.addEventListener("DOMContentLoaded", function(e){
 
         showCategoriesList();
     });
-
-        //   ENTREGA 4.3
-
-        const moonButton = document.getElementById("moonButton");
-
-        const darkMode = () => {
-          document.querySelector("body").setAttribute("data-bs-theme", "dark");
-          document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill"); //  cambia el icono 
-        };
-      
-        const lightMode = () => {
-          document.querySelector("body").setAttribute("data-bs-theme", "light");
-          document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
-        };
-      
-        const changeTheme = () => {
-          const theme = document.querySelector("body").getAttribute("data-bs-theme");
-          if (theme === "light") {
-            darkMode();
-          } else {
-            lightMode();
-          }
-        };
-      
-        moonButton.addEventListener("click", changeTheme);
         
 });
