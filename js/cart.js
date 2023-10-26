7abfb4cb2269fc7c79d1e08d572cf02127944a6b
vanesuarez <vanesuarez2301@gmail.com>
2023-10-25 22:26:24 -0300

    subtotals update

--- js/cart.js
+++ js/cart.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const finalPrice = document.getElementById("totalPrice");
   const radioButtons = document.querySelectorAll('input[name="exampleRadios"]');
 
-  // Llamamos el carrito desde almacenamiento local o crea un carrito vacio si no existe
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
   // Recorre los productos en el carrito y los muestra con la funcion createCartItem
@@ -16,7 +15,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 
-  // Mostrar el producto por defecto con el Fetch
   async function fetchAndDisplayProduct(url) {
     try {
       const response = await fetch(url);
@@ -27,7 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
       const cost = `${data.articles[0].currency} ${data.articles[0].unitCost}`;
       const count = `${data.articles[0].count}`;
 
-      // Crear un objeto para el producto del fetch
       const newProduct = {
         img: img,
         name: name,
@@ -35,7 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
         count: count,
       };
 
-      // Agregar el producto del fetch al carrito
       cart.push(newProduct);
 
       const article = createCartItem(newProduct);
@@ -47,7 +43,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   fetchAndDisplayProduct(url);
 
-  // Función para crear un elemento de carrito de compra
   function createCartItem(product) {
     const article = document.createElement("div");
     article.classList.add("divCartN");
@@ -67,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cCount = document.createElement("input");
     cCount.type = "number";
     cCount.min = "0";
-    cCount.value = product.quantity || 1; // Establecer el valor en 1 si no se proporciona una cantidad
+    cCount.value = product.quantity || 1;
     article.appendChild(cCount);
 
     const individualSubtotal = document.createElement("p");
@@ -75,61 +70,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Función para actualizar el subtotal individual y total cuando cambia la cantidad
     function updateSubtotal() {
+      const unitCost = parseFloat(product.price.replace(/[^0-9.-]+/g, ""));
+      const count = parseFloat(cCount.value) || 0;
+      const subtotal = unitCost * count;
+
+      individualSubtotal.innerHTML = `<b>${product.price.substring(0, 4)} ${subtotal.toFixed(2)}</b>`;
+
       let total = 0;
 
-      // Recorrer el carrito para calcular el subtotal individual y la suma total
-      cart.forEach((product) => {
-        const unitCost = parseFloat(product.price.replace(/[^0-9.-]+/g, ""));
-        const count = parseFloat(cCount.value) || 0;
-        const subtotal = unitCost * count;
-        total += subtotal;
-
-        // Actualiza el subtotal individual tomando la moneda y el precio
-        individualSubtotal.innerHTML = `<b>${product.price.substring(
-          0,
-          4
-        )} ${subtotal.toFixed(2)}</b>`;
-
-        // Actualizar el precio subtotal total en la sección de Costos
-        subtotalPrice.textContent = `USD ${total.toFixed(2)}`;
-
-        // Función para obtener y actualizar el costo de envio en tiempo real
-        function updateShippingPrice() {
-          // Obtenemos la opcion de envio seleccionada
-          const selectedShippingOption = document.querySelector(
-            'input[name="exampleRadios"]:checked'
-          );
-
-          if (selectedShippingOption) {
-            const shippingPercentage =
-              parseFloat(selectedShippingOption.value) / 100;
-            shippingPrice.textContent = `USD ${(
-              total * shippingPercentage
-            ).toFixed(2)}`;
-          } 
-        }
-
-        // Agregamos eventos change a los radio buttons con la funcion creada
-        radioButtons.forEach((radio) => {
-          radio.addEventListener("change", updateShippingPrice);
-        });
-
-        // Llamamos la funcion updateShippingPrice al cargar la pagina para establecer el precio de envio inicial
-        updateShippingPrice();
-
-        // Actualiza el precio total en tiempo real
-        finalPrice.textContent = `USD ${(
-          total + parseFloat(shippingPrice.textContent.split(" ")[1])
-        ).toFixed(2)}`;
+      // Recorrer el carrito para calcular la suma total
+      cart.forEach((item) => {
+        const itemUnitCost = parseFloat(item.price.replace(/[^0-9.-]+/g, ""));
+        const itemCount = parseFloat(item.count);
+        total += itemUnitCost * count;
       });
+
+      subtotalPrice.textContent = `USD ${total.toFixed(2)}`;
+
+      function updateShippingPrice() {
+        const selectedShippingOption = document.querySelector('input[name="exampleRadios"]:checked');
+
+        if (selectedShippingOption) {
+          const shippingPercentage = parseFloat(selectedShippingOption.value) / 100;
+          shippingPrice.textContent = `USD ${(total * shippingPercentage).toFixed(2)}`;
+        }
+      }
+
+      radioButtons.forEach((radio) => {
+        radio.addEventListener("change", updateShippingPrice);
+        radio.addEventListener("change", updateSubtotal);
+      });
+
+      updateShippingPrice();
+
+      finalPrice.textContent = `USD ${(parseFloat(subtotalPrice.textContent.split(" ")[1]) + parseFloat(shippingPrice.textContent.split(" ")[1])).toFixed(2)}`;
     }
 
-    // Llamar a la función updateSubtotal cuando cambia la cantidad y actualizar en tiempo real el subtotal individual
     cCount.addEventListener("input", updateSubtotal);
-
-    // Calcular el subtotal inicial
     updateSubtotal();
 
     return article;
   }
 });
+
+
+
