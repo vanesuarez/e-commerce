e6343b5ecbd9c5c8a432acdbb548644edfc90259
Mauricio Machado <133698730+machadokroyen@users.noreply.github.com>
2023-10-28 22:27:56 -0300

    testing3

--- js/cart.js
+++ js/cart.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cCount = document.createElement("input");
     cCount.type = "number";
     cCount.min = "0";
-    cCount.value = product.count; // Establece el valor en 1 si no se proporciona una cantidad
+    cCount.value = product.count || 1; // Establece el valor en 1 si no se proporciona una cantidad
     article.appendChild(cCount);
     cCount.addEventListener("input", () => {
       
