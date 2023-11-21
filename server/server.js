e9bf6c378cba82ba49ebff62de3de4835cbaf793
vanesuarez <vanesuarez2301@gmail.com>
2023-11-20 23:47:02 -0300

    json files added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(express.json());
+
+// Rutas estáticas para los archivos JSON
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
