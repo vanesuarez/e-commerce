ee67145650733bada9fc27936a740a03698b8725
vanesuarez <vanesuarez2301@gmail.com>
2023-11-21 00:07:51 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path');
 
 app.use(express.json());
-
-// Rutas estáticas para los archivos JSON
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
-
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
